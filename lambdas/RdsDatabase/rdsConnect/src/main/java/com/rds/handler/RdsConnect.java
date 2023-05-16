b70cace96eb4fb26563c4add4135c7d0d6959d6e
AKS-V02 <a.k.s02@outlook.com>
2023-05-17 01:51:11 +0530

    error while creating rds with proxy resolved

--- lambdas/RdsDatabase/rdsConnect/src/main/java/com/rds/handler/RdsConnect.java
+++ lambdas/RdsDatabase/rdsConnect/src/main/java/com/rds/handler/RdsConnect.java
@@ -2,6 +2,9 @@ package com.rds.handler;
 
 import java.sql.ResultSet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
@@ -14,47 +17,53 @@ import com.rds.utils.ExecuteStatement;
 public class RdsConnect implements RequestHandler<APIGatewayProxyRequestEvent,APIGatewayProxyResponseEvent>{
     APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
     ExecuteStatement executeStatement = new ExecuteStatement();
+    public static final Logger LOGGER = LoggerFactory.getLogger(RdsConnect.class);
     Gson gson = new Gson();
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
         
         try {
+            LOGGER.info("Handle Request Starts");
             JsonObject inputObj = JsonParser.parseString(input.getBody()).getAsJsonObject();
             String action = input.getPathParameters().get("proxy"); 
             executeStatement.startConnection();
             if(action.equalsIgnoreCase("/create-table")){
                 int result = executeStatement.createTable(inputObj.get("tableName").getAsString(), 
                                                                     inputObj.get("columnsList").getAsJsonArray());
-                executeStatement.closeConnection();
+                // executeStatement.closeConnection();
                 return response.withStatusCode(200).withBody("Table Created result"+result).withHeaders(ExecuteStatement.getHeaders()); 
             } else if (action.equalsIgnoreCase("/add-records")){
                  int result = executeStatement.addRecordsIntoTable(inputObj.get("tableName").getAsString(),
                                                      inputObj.get("columnsNameList").getAsJsonArray(), 
                                                      inputObj.get("columnsvalueList").getAsJsonArray());
-                executeStatement.closeConnection();
+                // executeStatement.closeConnection();
                 return response.withStatusCode(200).withBody("Recordes added sucessfully with result "+result).withHeaders(ExecuteStatement.getHeaders()); 
             }else if(action.equalsIgnoreCase("/update-records")){
                  int result = executeStatement.updateRecordOfTable(inputObj.get("tableName").getAsString(),
                                                      inputObj.get("conditionColumnValue").getAsJsonObject(), 
                                                      inputObj.get("updateColumnValue").getAsJsonObject());
-                executeStatement.closeConnection();
+                // executeStatement.closeConnection();
                 return response.withStatusCode(200).withBody("Records Updated With Result"+result).withHeaders(ExecuteStatement.getHeaders()); 
             }else if(action.equalsIgnoreCase("/get-records")){
                 ResultSet result = executeStatement.getAllColumnRecord(inputObj.get("tableName").getAsString()
                                                                         , inputObj.get("offset").getAsString(), 
                                                                         inputObj.get("limit").getAsString());
-                executeStatement.closeConnection();
+                // executeStatement.closeConnection();
                 return response.withStatusCode(200).withBody(gson.toJson(result)).withHeaders(ExecuteStatement.getHeaders()); 
             } else if(action.equalsIgnoreCase("/get-total-count")){
                 ResultSet result = executeStatement.getTotalRecordCount(inputObj.get("tableName").getAsString());
-                executeStatement.closeConnection();
+                // executeStatement.closeConnection();
                 return response.withStatusCode(200).withBody(gson.toJson(result)).withHeaders(ExecuteStatement.getHeaders()); 
             }
-            executeStatement.closeConnection();
+            // executeStatement.closeConnection();
             return response.withStatusCode(404).withBody("Wrong Path").withHeaders(ExecuteStatement.getHeaders()); 
         } catch (Exception e) {
-            executeStatement.closeConnection();
+            // executeStatement.closeConnection();
+            LOGGER.error("Exception"+e);
             return response.withStatusCode(400).withBody(e.getMessage()).withHeaders(ExecuteStatement.getHeaders());
+        } finally {
+            executeStatement.closeConnection();
+            LOGGER.info("Connection closed");
         }
         
     }
